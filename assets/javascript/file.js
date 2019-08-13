e8abdb6fbe7dfc520cdc2f38547267f04cc5828b
Natalia Malcova <nataliamalkova@yahoo.com>
2019-08-12 21:06:25 -0400

    created html and css files

---
+++ assets/javascript/file.js
@@ -0,0 +1,71 @@
+var trainName = moment();
+var tDestination = moment();
+var tFrequency = moment();
+var nextArr = moment();
+var minAway = moment();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var createRow = function(data) {
+//     var tRow = $("<tr>");
+//     var trainName = $("<td>").text(data.TrainName);
+//     var destination = $("<td>").text(data.Destination);
+//     var frequency = $("<td>").text(data.Frequency);
+//     var nextArr = $("<td>").text(data.NextArrival);
+//     var minAway = $("<td>").text(data.MinutesAway);
+
+//     tRow.append(trainName, destination, frequency, nextArr, minAway);
+//     $("tbody").append(tRow);
+// };
+// console.log(createRow);
